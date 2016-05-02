bd1d344c80becb493d63117a9c412bc820fb5cb5
Ben Tan <ben.tan@snowballeffect.co.nz>
2016-05-02 22:56:03 +1200

    Added Vimeo to the mix

--- templates/post-title-video.php
+++ templates/post-title-video.php
@@ -1,7 +1,8 @@
 <?php
 
 $post_id = get_the_ID();
-$video_id = get_post_custom_values('youtube_video_id');
+$video_id_youtube = get_post_custom_values('youtube_video_id');
+$video_id_vimeo = get_post_custom_values('vimeo_video_id');
 
 ?>
 
@@ -18,10 +19,16 @@ $video_id = get_post_custom_values('youtube_video_id');
 						<!-- Article excerpt -->
 						<p class="banner__description text-center text-medium hidden"><span class="text-fancy">by</span> <strong><?= get_the_author(); ?></strong></p>
 
-						<?php if(!empty($video_id)) : ?>
+						<?php if(!empty($video_id_youtube)) : ?>
 						<div class="banner__video">
 							<div class="embed-responsive embed-responsive-16by9 m-t-30">
-								<iframe class="embed-responsive-item" width="816" height="459" src="https://www.youtube.com/embed/zpOULjyy-n8" frameborder="0" allowfullscreen></iframe>
+								<iframe class="embed-responsive-item" width="816" height="459" src="https://www.youtube.com/embed/<?= $video_id_youtube ?>" frameborder="0" allowfullscreen></iframe>
+							</div>
+						</div>
+						<?php elseif(!empty($video_id_vimeo)) : ?>
+						<div class="banner__video">
+							<div class="embed-responsive embed-responsive-16by9 m-t-30">
+								<iframe src="https://player.vimeo.com/video/<?= $video_id_vimeo ?>?title=0&byline=0&portrait=0&badge=0" class="embed-responsive-item" width="816" height="459" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 							</div>
 						</div>
 						<?php endif; ?>
