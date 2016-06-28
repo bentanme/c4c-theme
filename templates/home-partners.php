7dd0bba4f119719cd0279d8d976a6f8b25c82f3d
Ben Tan <ben.tan@snowballeffect.co.nz>
2016-06-28 22:24:32 +1200

    Remove breeze from sponsors

--- templates/home-partners.php
+++ templates/home-partners.php
@@ -21,7 +21,7 @@
 					<li><a href="http://www.haier.co.nz" target="_blank"><img src="<?= get_bloginfo('template_url') ?>/dist/images/sponsors/sponsors_haier.png" alt="Haier"></a></li>
 					<li><a href="http://www.2degreesmobile.co.nz" target="_blank"><img src="<?= get_bloginfo('template_url') ?>/dist/images/sponsors/sponsors_2degrees.png" alt="2Degrees"></a></li>
 					<li><a href="http://www.whitestonecheese.com" target="_blank"><img src="<?= get_bloginfo('template_url') ?>/dist/images/sponsors/sponsors_whitestone-cheese.png" alt="Whitestone Cheese Co"></a></li>
-					<li><a href="http://www.thebreeze.co.nz" target="_blank"><img src="<?= get_bloginfo('template_url') ?>/dist/images/sponsors/sponsors_the-breeze.png" alt="The Breeze"></a></li>
+					<!-- <li><a href="http://www.thebreeze.co.nz" target="_blank"><img src="<?= get_bloginfo('template_url') ?>/dist/images/sponsors/sponsors_the-breeze.png" alt="The Breeze"></a></li> -->
 					<li><a href="http://www.jucy.co.nz" target="_blank"><img src="<?= get_bloginfo('template_url') ?>/dist/images/sponsors/sponsors_jucy.png" alt="Jucy Rentals"></a></li>
 					<li><a href="http://www.canon.co.nz" target="_blank"><img src="<?= get_bloginfo('template_url') ?>/dist/images/sponsors/sponsors_canon.png" alt="Canon"></a></li>
 					<li><a href="http://www.jennian.co.nz" target="_blank"><img src="<?= get_bloginfo('template_url') ?>/dist/images/sponsors/sponsors_jennian-homes.png" alt="Jennian Homes"></a></li>
